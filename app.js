ba2dc6ac10d5ede48007aaca2cfb3397f8343171
Fabian Juren <fjuren@gmail.com>
2022-06-03 08:49:10 -0700

    Reorg. code

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// global identifiers
-const burg = document.getElementById("navBurger-item")
+// Navbar identifiers
+const menuBtn = document.getElementById("navBurger-item")
+const navMenuContent = document.getElementById("nav-menu");
  
 // menu subcontent identifiers
-const wbtn = document.getElementById("w-btn");
+const wBtn = document.getElementById("w-btn");
 const wcontent = document.getElementById("women-content");
-const mbtn = document.getElementById("m-btn");
+const mBtn = document.getElementById("m-btn");
 const mcontent = document.getElementById("men-content");
-const sbtn = document.getElementById("s-btn");
+const sBtn = document.getElementById("s-btn");
 const scontent = document.getElementById("sale-content");
-const menuButton = document.getElementById("navBurger-item")
-const navMenuContent = document.getElementById("nav-menu");
+
 
 // 
 // function for button clicks
@@ -20,7 +20,7 @@ const btnClicks = (anyButton, anyContent) => {
 }
 
 // ID -> Style
-// toggles display of menu on/off. Helper function for any button
+// toggles display of menu on/off. Extends functionality from btnClicks
 const toggleMenus = (content) => {
     if(content.style.display !== "none") {
         content.style.display = "none";
@@ -29,7 +29,7 @@ const toggleMenus = (content) => {
     };
 };
 
-btnClicks(wbtn, wcontent); // click button and toggle women submenu
-btnClicks(mbtn, mcontent); // click button and toggle men submenu
-btnClicks(sbtn, scontent); // click button and toggle sale submenu
-btnClicks(menuButton, navMenuContent)
+btnClicks(menuBtn, navMenuContent) // click button and toggle nav menu
+btnClicks(wBtn, wcontent); // click button and toggle women submenu
+btnClicks(mBtn, mcontent); // click button and toggle men submenu
+btnClicks(sBtn, scontent); // click button and toggle sale submenu
