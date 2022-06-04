5af1b03d31d82a6326f1bc28da2754afae3ad1b3
Fabian Juren <fjuren@gmail.com>
2022-06-03 21:29:20 -0700

    Add closeDropdowns fcn. Closes any dropdown if open

--- Dropdown-menus/app.js
+++ Dropdown-menus/app.js
@@ -1,7 +1,7 @@
 // Navbar identifiers
-const menuBtn = document.getElementById("navBurger-item")
+const menuBtn = document.getElementById("navBurger-item");
 const navMenuContent = document.getElementById("nav-menu");
- 
+
 // menu subcontent identifiers
 const wBtn = document.getElementById("w-btn");
 const wcontent = document.getElementById("women-content");
@@ -10,26 +10,37 @@ const mcontent = document.getElementById("men-content");
 const sBtn = document.getElementById("s-btn");
 const scontent = document.getElementById("sale-content");
 
+// all submenu content
+const subDropdown = document.getElementsByClassName("subnav-content");
 
-// 
-// function for button clicks
+// ID ID -> Style Style
+// applies onclick to any btn
 const btnClicks = (anyButton, anyContent) => {
-    anyButton.onclick = () => {
-        toggleMenus(anyContent)
-    }
-}
-
-// ID -> Style
-// toggles display of menu on/off. Extends functionality from btnClicks
-const toggleMenus = (content) => {
-    if(content.style.display !== "none") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block"
-    };
+  anyButton.onclick = () => {
+    closeDropdowns(subDropdown);
+    toggleMenus(anyContent);
+  };
 };
 
-btnClicks(menuBtn, navMenuContent) // click button and toggle nav menu
+// ID -> Style
+// toggles display of specific menu on/off. Extends functionality from btnClicks
+const toggleMenus = (content) => {
+  if (content.style.display !== "none") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+};
+
+// Array -> Style
+// Closes all submenu dropdowns. Extends functionality from btnClicks
+const closeDropdowns = (dropDowns) => {
+  for (let i = 0; i < dropDowns.length; i++) {
+    dropDowns[i].style.display = "none";
+  }
+};
+
+btnClicks(menuBtn, navMenuContent); // click button and toggle nav menu
 btnClicks(wBtn, wcontent); // click button and toggle women submenu
 btnClicks(mBtn, mcontent); // click button and toggle men submenu
 btnClicks(sBtn, scontent); // click button and toggle sale submenu
